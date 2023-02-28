29b7033f07f6ef1f259e46bc5074b4723c359fb2
LeAnh <leanhxtech@gmail.com>
2023-03-01 00:07:09 +0700

    Small Update

--- spring-boot-ecommerce/src/main/java/com/ptit/springbootdepartmentstore/entity/Orders.java
+++ spring-boot-ecommerce/src/main/java/com/ptit/springbootdepartmentstore/entity/Orders.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -38,6 +39,6 @@ public class Orders {
 	@JoinColumn(name="user_id", nullable=false)
 	private User user;
 	
-    @OneToMany(mappedBy = "orderDetailId.order")
+    @OneToMany(mappedBy = "id.order")
 	private List<OrderDetail> orderDetailList;
 }
